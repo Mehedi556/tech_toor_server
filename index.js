8b39e8030c945d1a4dd2eba91fd445a5c2fee57d
mehedi556 <mehe556di@gmail.com>
2022-10-27 07:30:19 +0600

    making API

--- index.js
+++ index.js
@@ -1,6 +1,40 @@
 const express = require('express')
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const categories = require('./data/options.json');
+
+const details = require('./data/details.json');
+
+app.get('/categories' , (req , res) => {
+    res.send(categories)
+})
+
+app.get('/details/:id' , (req , res) => {
+    const id = req.params.id;
+    const selectedNews = details.find( detail => detail.id === id);
+    res.send(selectedNews)
+})
+
+app.get('/' , (req , res) => {
+    res.send('news api running')
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 app.listen(port , () => {
