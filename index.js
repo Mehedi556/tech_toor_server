264529ffc4968c34c711e9a4850ed69ef6d3306e
mehedi556 <mehe556di@gmail.com>
2022-10-27 14:41:53 +0600

    adding cart details for detail option

--- index.js
+++ index.js
@@ -10,13 +10,17 @@ const categories = require('./data/options.json');
 const details = require('./data/details.json');
 
 app.get('/categories' , (req , res) => {
-    res.send(categories)
+    res.send(categories);
 })
 
 app.get('/details/:id' , (req , res) => {
     const id = req.params.id;
     const selectedNews = details.find( detail => detail.id === id);
     res.send(selectedNews)
+})
+
+app.get('/details' , (req , res) => {
+    res.send(details);
 })
 
 app.get('/' , (req , res) => {
